bf2cee1c23088a4e1cd7bba8659d8cebc32ac550
Rob VanCleave <rob.vancleave@cyberark.com>
2025-08-21 15:39:32 -0400

    added the logo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,8 @@ import './Hero.css';
 function Hero() {
   return (
     <div className="hero d-flex align-items-center justify-content-center text-center">
-      <div className="hero-text text-white">
-        <h1 className="display-3">Halloween Bash 2025</h1>
-        <p className="lead">Join us for a night of thrills and chills!</p>
+      <div className="hero-logo">
+        
       </div>
     </div>
   );
